5f2cc9af7e580f5df2c6b7f003d07e6a53ac4f3e
Daler <khabibov_dk@outlook.com>
2017-09-18 16:09:10 +0600

    initilize repo | first gen files

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+	var PORT = process.env.PORT || 3000;
+	
+app.get('/', function(req, res){
+	app.use(express.static(__dirname + '/pages'));
+    res.sendFile('pages/index.html', {root: __dirname })
+});
+
+app.listen(PORT, function(){
+	console.log('Local server started at PORT: ' + PORT + '!');
+});
