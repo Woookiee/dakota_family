4428eb97da010e31ba048b68a5ce0a55d4aad9c1
Daler <khabibov_dk@outlook.com>
2017-09-19 23:14:39 +0600

    testing second button4

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var express = require('express');
 var app = express();
 	var PORT = process.env.PORT || 3000;
 	
+app.use(express.static(__dirname + '/pages'));
+
 app.get('/', function(req, res){
-	app.use(express.static(__dirname + '/pages'));
     res.sendFile('pages/index.html', {root: __dirname })
 });
 
-app.get('/about', function(req, res){
+app.get('/rules', function(req, res){
     res.sendFile('pages/rules.html', {root: __dirname })
 });
 
