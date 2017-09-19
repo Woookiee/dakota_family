599e84c247d997d05cba6f82c099201702d12deb
Daler <khabibov_dk@outlook.com>
2017-09-19 22:58:50 +0600

    testing second button2

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get('/', function(req, res){
     res.sendFile('pages/index.html', {root: __dirname })
 });
 
+app.get('/rules', function(req, res){
+	app.use(express.static(__dirname + '/pages'));
+    res.sendFile('pages/rules.html', {root: __dirname })
+});
+
 app.listen(PORT, function(){
 	console.log('Local server started at PORT: ' + PORT + '!');
 });
